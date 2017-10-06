8efe3fa94fa604680864e87fcef2b487713e392e
Charles Korn <me@charleskorn.com>
2017-10-06 14:28:25 +1100

    Push image with a tag.

--- dev-infrastructure/buildAndPushImage.sh
+++ dev-infrastructure/buildAndPushImage.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+TAG=$(git rev-parse HEAD)
+
 echo $DOCKER_PASSWORD | docker login --username $DOCKER_USER --password-stdin
-docker build -t charleskorn/batect-sample dev-infrastructure/international-transfers-service
-docker push charleskorn/batect-sample
+docker build -t charleskorn/batect-sample:latest dev-infrastructure/international-transfers-service
+docker tag charleskorn/batect-sample:latest charleskorn/batect-sample:$TAG
+docker push charleskorn/batect-sample:latest
+docker push charleskorn/batect-sample:$TAG
