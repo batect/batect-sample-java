1490b7604cf263c3645734b0fb8b6748c18516ee
Charles Korn <me@charleskorn.com>
2018-04-01 19:57:47 +1000

    Use new project name for Docker repository name.

--- dev-infrastructure/buildAndPushImage.sh
+++ dev-infrastructure/buildAndPushImage.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 TAG=$(git rev-parse HEAD)
 
 echo $DOCKER_PASSWORD | docker login --username $DOCKER_USER --password-stdin
-docker build -t charleskorn/batect-sample:latest dev-infrastructure/international-transfers-service
-docker tag charleskorn/batect-sample:latest charleskorn/batect-sample:$TAG
-docker push charleskorn/batect-sample:latest
-docker push charleskorn/batect-sample:$TAG
+docker build -t charleskorn/batect-sample-java:latest dev-infrastructure/international-transfers-service
+docker tag charleskorn/batect-sample-java:latest charleskorn/batect-sample-java:$TAG
+docker push charleskorn/batect-sample-java:latest
+docker push charleskorn/batect-sample-java:$TAG
