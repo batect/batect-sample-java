591aae0e72005b79dd5e3d0259297ceb2a9909d0
Charles Korn <me@charleskorn.com>
2019-05-05 21:31:38 +1000

    Adopt .batect naming convention.
    See https://github.com/charleskorn/batect/issues/79.

--- .batect/buildAndPushImage.sh
+++ .batect/buildAndPushImage.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 TAG=$(git rev-parse HEAD)
 
 echo $DOCKER_PASSWORD | docker login --username $DOCKER_USER --password-stdin
-docker build -t charleskorn/batect-sample-java:latest dev-infrastructure/international-transfers-service
+docker build -t charleskorn/batect-sample-java:latest .batect/international-transfers-service
 docker tag charleskorn/batect-sample-java:latest charleskorn/batect-sample-java:$TAG
 docker push charleskorn/batect-sample-java:latest
 docker push charleskorn/batect-sample-java:$TAG
