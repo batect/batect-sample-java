d0d33b4a84b130bca163e04a109fbbe85b33fed9
Charles Korn <me@charleskorn.com>
2021-09-24 13:22:39 +1000

    Remove Travis CI and use GitHub Actions instead.
    See https://arstechnica.com/information-technology/2021/09/travis-ci-flaw-exposed-secrets-for-thousands-of-open-source-projects.

--- .batect/buildAndPushImage.sh
+++ .batect/buildAndPushImage.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 TAG=$(git rev-parse HEAD)
 
 echo $DOCKER_PASSWORD | docker login --username $DOCKER_USER --password-stdin
-docker build -t charleskorn/batect-sample-java:latest .batect/international-transfers-service
-docker tag charleskorn/batect-sample-java:latest charleskorn/batect-sample-java:$TAG
-docker push charleskorn/batect-sample-java:latest
-docker push charleskorn/batect-sample-java:$TAG
+docker build -t batect/batect-sample-java:latest .batect/international-transfers-service
+docker tag batect/batect-sample-java:latest batect/batect-sample-java:$TAG
+docker push batect/batect-sample-java:latest
+docker push batect/batect-sample-java:$TAG
